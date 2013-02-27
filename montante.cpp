5dd51ed49f9ef4bc6ec53a7be86437d2469ba9de
Jose Esquivel <beto.esquivel.p@gmail.com>
2013-02-27 10:46:29 -0600

    Comenzando...

--- montante.cpp
+++ montante.cpp
@@ -1,1 +1,15 @@
 #include <iostream>
+
+bool debug = false; 
+
+void Montante(double pAnterior, double pActual, double mat[][]){
+	
+}
+
+int main(){
+	
+	double pivoteAnterior = 1; 
+	double pivoteActual = 1; 
+	
+	return 0; 
+}
